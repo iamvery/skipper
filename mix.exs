7eb04fe18ddd50f28df4ec041a0fc148e38e2e03
Jay Hayes <ur@iamvery.com>
2023-05-05 07:48:42 -0500

    Port over some mix.exs changes from iamvery package

--- mix.exs
+++ mix.exs
@@ -1,12 +1,28 @@
 defmodule Skipper.MixProject do
   use Mix.Project
 
+  @description """
+  LiveView tests tend to leak details that distract from the system behavior
+  you're testing. Skip the noise and let the Skipper patch the leaks so you can
+  sail on in your well-tested craft.
+  """
+
+  @package [
+    files: ["lib", "mix.exs", "README*"],
+    maintainers: ["Jay Hayes"],
+    licenses: ["MIT"],
+    links: %{"GitHub" => "https://github.com/iamvery/skipper"}
+  ]
+
   def project do
     [
       app: :skipper,
       version: "0.1.0",
-      elixir: "~> 1.14",
+      elixir: "~> 1.12",
+      elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
+      description: @description,
+      package: @package,
       deps: deps()
     ]
   end
@@ -17,6 +33,9 @@ defmodule Skipper.MixProject do
       extra_applications: [:logger]
     ]
   end
+
+  defp elixirc_paths(:test), do: ["lib", "test/support"]
+  defp elixirc_paths(_), do: ["lib"]
 
   # Run "mix help deps" to learn about dependencies.
   defp deps do
